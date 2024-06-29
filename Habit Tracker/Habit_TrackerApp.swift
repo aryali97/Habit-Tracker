f8fecbf1ef82365e05cfdfb8b03e20b059b25c15
Anirudh Ryali <aryali97@gmail.com>
2024-06-29 17:11:08 -0400

    Initial Commit

---
+++ Habit Tracker/Habit_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_TrackerApp.swift
+//  Habit Tracker
+//
+//  Created by Anirudh Ryali on 6/29/24.
+//
+
+import SwiftUI
+
+@main
+struct Habit_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
